04100d9f601295aa67f7b38a81743f0e67866091
Harsh <harsh.work8421@gmail.com>
2024-08-26 13:41:02 +0530

    app.py and .env file updated

---
+++ app.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+import pandas as pd
+import streamlit as st 
+import os
+from PIL import Image
+import pdf2image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input, pdf_content,prompt):
+    model=genai.GenerativeModel('gemini-1.5-flash')
+    response=model.generate_content(input,pdf_content[0],prompt)
+    return response.text
