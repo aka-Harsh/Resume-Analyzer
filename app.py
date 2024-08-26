236c95cafdabfb41c906772b2d34d667caec3609
Harsh <harsh.work8421@gmail.com>
2024-08-26 17:36:37 +0530

    app.py is updated

--- app.py
+++ app.py
@@ -13,3 +13,27 @@ def get_gemini_response(input, pdf_content,prompt):
     model=genai.GenerativeModel('gemini-1.5-flash')
     response=model.generate_content(input,pdf_content[0],prompt)
     return response.text
+
+def input_pdf_setup(uploaded_files):
+    if uploaded_files is not None:
+        images=pdf2image.convert_from_bytes(uploaded_files.read())
+
+        first_page=images[0]
+
+        img_byte_arr =io.BytesIO()
+        first_page.save(img_byte_arr,format='JPEG')
+        img_byte_arr=img_byte_arr.getvalue()
+
+        pdf_parts=[
+            {
+            "mime_type":"image/jpeg",
+            "data":base64.b64encode(img_byte_arr).decode()
+            }
+        ]
+        return pdf_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+st.set_page_config(page_title="Resume Analyzer")
+st.header("Analyze your Resume and check ATS score")
+
